0fb05f8ae0b5c172be8954fa1158fb3df1ae6950
Aleksandr <daaarkzs47@gmail.com>
2025-06-15 14:33:09 +0500

    MUI components for Header

---
+++ src/MUI components/SearchField.jsx
@@ -0,0 +1,36 @@
+import {alpha, styled} from "@mui/material";
+import SearchIconWrapper from "./SearchIconWrapper.jsx";
+import SearchIcon from "@mui/icons-material/Search";
+import StyledInputBase from "./StyledInputBase.jsx";
+
+const Search = styled('div')(({theme}) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    },
+}));
+
+const SearchField = () => {
+    return (
+        <Search>
+            <SearchIconWrapper>
+                <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+            />
+        </Search>
+    );
+}
+
+export default SearchField;
