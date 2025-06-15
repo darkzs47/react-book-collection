afaaf24f051141ab306f3a2a94cee71c54343fb9
Aleksandr <daaarkzs47@gmail.com>
2025-06-15 14:38:58 +0500

    App component with Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {lightGreen, common} from '@mui/material/colors';
+import Book from './Pages/Book.jsx'
+import Settings from './Pages/Settings.jsx'
+import Main from "./Pages/Main.jsx";
+import Header from "./Components/Header.jsx";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: common.black
+        },
+        secondary: {
+            main: lightGreen['A400']
+        },
+    },
+});
 
 function App() {
 
-  return (
-    <>
+    return (
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Header/>
 
-    </>
-  )
+                <Routes>
+                    <Route path="/" element={<Main/>}/>
+                    <Route path="/book/:id" element={<Book/>}/>
+                    <Route path="/settings" element={<Settings/>}/>
+                </Routes>
+
+            </Router>
+        </ThemeProvider>
+    )
 }
 
 export default App
